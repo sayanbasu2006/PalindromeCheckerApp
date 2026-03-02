726a609c071b44787bafe72860d9b7fc80677b62
sayanbasu2006 <sayanbasu2006@gmail.com>
2026-03-02 14:57:38 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * =========================================================
  * MAIN CLASS - UseCase1PalindromeCheckerApp
@@ -33,10 +34,21 @@ public class PalindromeCheckerApp {
      * @param args Command-line arguments
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input Text: ");
+        String name = scanner.nextLine();
+        int j = name.length() - 1;
+        boolean isPalindrome = true;
+        for (int i = 0; i < name.length() / 2; i++) {
 
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+            if (name.charAt(i) != name.charAt(j)) {
+                isPalindrome = false;
+                break;   // stop checking once mismatch found
+            }
 
+            j--;  // move backward
+        }
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+        scanner.close();
     }
 }
